30b7212e91a1ab27acc0d0794f9dc4379c29dda2
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-05-29 16:47:19 -0500

    wrote hello world function and called the function

--- Exercise Files/Ch2/helloworld_start.py
+++ Exercise Files/Ch2/helloworld_start.py
@@ -1,4 +1,19 @@
 #
 # Example file for HelloWorld
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
-print "Hello World"
+# print "Hello World"
+
+# Defining a function called main and it only has one statement
+# nothing happens when u call this function because you need to call it
+def main():
+  print "hello world!"
+if __name__ == "__main__":
+  main()
+# line 10 is checking to see if this line of code is loaded __name__ value of main
+# therefore this function main should be called.
+# helps show the difference when python code is being executed as a library vs program
+
+print "me too"
+
+# indicated lines 9 belongs to function main
+# python uses the indentation level of the lines of code to indicate where the code belongs
